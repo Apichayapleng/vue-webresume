2479640ba2fa01c49269f9b66ca82b6ee48e130d
Apichaya <apichayapg@gmail.com>
2018-11-14 17:02:39 +0700

    add content

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="main">
+    <!-- <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -28,7 +28,9 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
+    
+    <p>Hii~~</p>
   </div>
 </template>
 
@@ -36,25 +38,25 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
-export default class HelloWorld extends Vue {
+export default class mainPage extends Vue {
   @Prop() private msg!: string;
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+// h3 {
+//   margin: 40px 0 0;
+// }
+// ul {
+//   list-style-type: none;
+//   padding: 0;
+// }
+// li {
+//   display: inline-block;
+//   margin: 0 10px;
+// }
+// a {
+//   color: #42b983;
+// }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <mainPage msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <!-- <mainPage/> -->
+    
+    <div class="sidenav">
+      <a href="#">about</a>
+      <a href="#">edu</a>
+      <a href="#">work</a>
+      <a href="#">contact</a>
+    </div>
+    <div class="main">
+      <div class="about">
+        <p>I am fourth year student who interested in web frontend developer and ui/ux design. I am seeking for an co-operative or internship about 4-6 months periods</p>
+      </div>
+      <div class="edu">
+        <p><br><br>King Mongkut's Institute of Technology Ladkrabang (KMITL)<br>
+          4th year student, GPA 3.04<br>
+          Bachelor of Science, International College,<br>
+          Engineering and Technology Management<br><br>
+
+          Poznan University of Economics<br>
+          Summer School (one month period, July 2016)<br><br>
+
+          Shigally Hill International Academy, India (2 years)<br><br>
+
+          Sarasas Witead Rangsit School</p>
+      </div>
+      <div class="work">
+        <p><br><br>Socket9<br>
+          Web frontend developer<br>
+          May 2017 - June 2017 (1 month)<br><br>
+
+          Codium<br>
+          Web frontend developer and designer<br>
+          June 2018 - July 2017 (2 month)<br><br>
+          </p>
+      </div>
+      <div class="contact">
+        <p><br><br>apichayapg@gmail.com<br>
+          098-4649892 <br><br> </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import mainPage from '@/components/mainPage.vue' // @ is an alias to /src
 
 @Component({
   components: {
-  HelloWorld,
+  mainPage,
   },
   })
 export default class Home extends Vue {}
 </script>
+
+<style scoped lang="scss">
+
+.sidenav {
+    height: 100%; /* Full-height: remove this if you want "auto" height */
+    width: 160px; /* Set the width of the sidebar */
+    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1; /* Stay on top */
+    top: 0; /* Stay at the top */
+    left: 0;
+    overflow-x: hidden; /* Disable horizontal scroll */
+    // padding-top: 20px;
+}
+
+/* The navigation menu links */
+.sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+}
+
+/* When you mouse over the navigation links, change their color */
+.sidenav a:hover {
+    color: black;
+}
+
+/* Style page content */
+.main {
+    margin-left: 160px; /* Same as the width of the sidebar */
+    padding: 0px 10px;
+}
+
+</style>
