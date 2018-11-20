05eb90c26718921d433087b26449569c33a1fdc8
Apichaya <apichayapg@gmail.com>
2018-11-20 09:53:00 +0700

    add profile pic and sidenav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <mainPage msg="Welcome to Your Vue.js + TypeScript App"/> -->
-    <!-- <mainPage/> -->
-    
     <div class="sidenav">
-      <a href="#">about</a>
-      <a href="#">edu</a>
-      <a href="#">work</a>
-      <a href="#">contact</a>
+      <a href="#about" class>about</a>
+      <a href="#edu" class>edu</a>
+      <a href="#work" class>work</a>
+      <a href="#contact" class>contact</a>
     </div>
     <div class="main">
-      <div class="about">
+      <section class="section1" id="about">
+        <img src="../assets/profile.jpg">
         <p>I am fourth year student who interested in web frontend developer and ui/ux design. I am seeking for an co-operative or internship about 4-6 months periods</p>
-      </div>
-      <div class="edu">
+      </section>
+      <section class="section2" id="edu">
         <p><br><br>King Mongkut's Institute of Technology Ladkrabang (KMITL)<br>
           4th year student, GPA 3.04<br>
           Bachelor of Science, International College,<br>
@@ -26,8 +23,8 @@
           Shigally Hill International Academy, India (2 years)<br><br>
 
           Sarasas Witead Rangsit School</p>
-      </div>
-      <div class="work">
+      </section>
+      <section class="section3" id="work">
         <p><br><br>Socket9<br>
           Web frontend developer<br>
           May 2017 - June 2017 (1 month)<br><br>
@@ -36,11 +33,11 @@
           Web frontend developer and designer<br>
           June 2018 - July 2017 (2 month)<br><br>
           </p>
-      </div>
-      <div class="contact">
+      </section>
+      <section class="section4" id="contact">
         <p><br><br>apichayapg@gmail.com<br>
           098-4649892 <br><br> </p>
-      </div>
+      </section>
     </div>
   </div>
 </template>
@@ -63,11 +60,13 @@ export default class Home extends Vue {}
     height: 100%; /* Full-height: remove this if you want "auto" height */
     width: 160px; /* Set the width of the sidebar */
     position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-    z-index: 1; /* Stay on top */
-    top: 0; /* Stay at the top */
+    // z-index: 0; /* Stay on top */
+    // top: 0; /* Stay at the top */
     right: 0;
     overflow-x: hidden; /* Disable horizontal scroll */
     // padding-top: 20px;
+    text-align: center;
+
 }
 
 /* The navigation menu links */
