5e61664c4c232b9d9f258df3db2376fab49751e7
Apichaya <apichayapg@gmail.com>
2018-11-20 20:56:53 +0700

    add header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,45 @@
 <template>
   <div class="home">
     <div class="sidenav">
-      <a href="#about" class>about</a>
-      <a href="#edu" class>edu</a>
-      <a href="#work" class>work</a>
-      <a href="#contact" class>contact</a>
+      <a href="#about" class>About</a>
+      <a href="#edu" class>Education</a>
+      <a href="#work" class>Experience</a>
+      <a href="#contact" class>Contact</a>
     </div>
     <div class="main">
       <section class="section1" id="about">
-        <img src="../assets/profile.jpg">
-        <p>I am fourth year student who interested in web frontend developer and ui/ux design. I am seeking for an co-operative or internship about 4-6 months periods</p>
+
+      <div class="row">
+        <!-- <b-row> -->
+          <div class="col-6"><img src="../assets/profile.jpg" width="400px"></div>
+          <div class="col-4">
+            <h1>APICHAYA HUANGRAK</h1>
+            
+            <p>I am fourth year student who interested in web frontend developer and ui/ux design. I am seeking for an co-operative or internship about 4-6 months periods</p>
+          </div>
+        <!-- </b-row> -->
+      </div>
+
+        <!-- <img src="../assets/profile.jpg" width="300px">
+        <p>I am fourth year student who interested in web frontend developer and ui/ux design. I am seeking for an co-operative or internship about 4-6 months periods</p> -->
       </section>
       <section class="section2" id="edu">
-        <p><br><br>King Mongkut's Institute of Technology Ladkrabang (KMITL)<br>
+        <h2>EDUCATION</h2>
+        <p><b>King Mongkut's Institute of Technology Ladkrabang (KMITL)</b><br>
           4th year student, GPA 3.04<br>
           Bachelor of Science, International College,<br>
           Engineering and Technology Management<br><br>
 
-          Poznan University of Economics<br>
+          <b>Poznan University of Economics</b><br>
           Summer School (one month period, July 2016)<br><br>
 
-          Shigally Hill International Academy, India (2 years)<br><br>
+          <b>Shigally Hill International Academy, India (2 years)</b><br><br>
 
-          Sarasas Witead Rangsit School</p>
+          <b>Sarasas Witead Rangsit School</b></p>
       </section>
       <section class="section3" id="work">
-        <p><br><br>Socket9<br>
+        <h2><br><br>WORK EXPERIENCE</h2>
+        <p><b>Socket9</b><br>
           Web frontend developer<br>
           May 2017 - June 2017 (1 month)<br><br>
 
@@ -35,7 +49,8 @@
           </p>
       </section>
       <section class="section4" id="contact">
-        <p><br><br>apichayapg@gmail.com<br>
+        <h2>CONTACT</h2>
+        <p>apichayapg@gmail.com<br>
           098-4649892 <br><br> </p>
       </section>
     </div>
@@ -85,6 +100,7 @@ export default class Home extends Vue {}
 
 /* Style page content */
 .main {
+  padding-top: 20px;
     // margin-left: 0px; /* Same as the width of the sidebar */
     // padding: 0px 10px;
 }
